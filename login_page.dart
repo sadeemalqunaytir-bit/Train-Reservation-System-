c604caa15b19ba0e80d6d3a2aee15f09e80a83a9
sadeemalqunaytir-bit <sadeemalqunaytir@gmail.com>
2026-04-11 17:31:53 +0300

    Add files via upload

---
+++ login_page.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController idController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void login() {
+    String id = idController.text.trim();
+    String password = passwordController.text.trim();
+
+
+    if (id.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Please enter your ID")),
+      );
+      return;
+    }
+
+
+    if (password.length < 6 || !RegExp(r'[a-zA-Z]').hasMatch(password)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            "Password must be at least 6 characters and contain at least one letter",
+          ),
+        ),
+      );
+      return;
+    }
+
+
+    if (id.startsWith('1')) {
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    } else if (id.startsWith('2')) {
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Invalid ID (must start with 1 for Admin or 2 for Staff)"),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Train Reservation System'),
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                const SizedBox(height: 20),
+
+                CircleAvatar(
+                  radius: 80,
+                  backgroundColor: Colors.purple.withOpacity(0.2),
+                  child: const Icon(
+                    Icons.train,
+                    color: Colors.purple,
+                    size: 100,
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                const Text(
+                  'Login Page',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+
+                SizedBox(
+                  height: 45,
+                  width: 300,
+                  child: TextField(
+                    controller: idController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'User ID',
+                      prefixIcon: const Icon(Icons.badge),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 15),
+
+
+                SizedBox(
+                  height: 45,
+                  width: 300,
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: const Icon(Icons.lock),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+
+                SizedBox(
+                  height: 45,
+                  width: 300,
+                  child: CupertinoButton(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(9),
+                    padding: EdgeInsets.zero,
+                    onPressed: login,
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 40),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
