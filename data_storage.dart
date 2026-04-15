89e162b1583eb5ddc4c6e0aef264ccbbe9a05cc5
najdalobaishi <najdalobaishi@gmail.com>
2026-04-15 19:23:55 +0300

    Update data_storage.dart

--- data_storage.dart
+++ data_storage.dart
@@ -3,8 +3,8 @@ class DataStorage {
   static final List<Map<String, dynamic>> trains = [];
   static final List<Map<String, dynamic>> schedules = [];
 
-
   static int bookingCounter = 1;
   static List<Map<String, dynamic>> bookings = [];
 
+  static List<Map<String, dynamic>> passengers = [];
 }
