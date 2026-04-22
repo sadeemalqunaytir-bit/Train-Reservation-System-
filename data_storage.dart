73e37d98dc5c0c1268eaa3c2b521d31abbb6c37f
sadeemalqunaytir-bit <sadeemalqunaytir@gmail.com>
2026-04-22 17:14:55 +0300

    Add train capacity and booking utility functions

--- data_storage.dart
+++ data_storage.dart
@@ -1,4 +1,3 @@
-
 class DataStorage {
   static final List<Map<String, dynamic>> trains = [];
   static final List<Map<String, dynamic>> schedules = [];
@@ -7,4 +6,19 @@ class DataStorage {
   static List<Map<String, dynamic>> bookings = [];
 
   static List<Map<String, dynamic>> passengers = [];
+}
+
+
+
+bool isTrainFull(Map<String, dynamic> schedule) {
+  final seats = int.tryParse(schedule['availableSeats'].toString()) ?? 0;
+  return seats <= 0;
+}
+
+int seatsLeft(Map<String, dynamic> schedule) {
+  return int.tryParse(schedule['availableSeats'].toString()) ?? 0;
+}
+
+int capacity(Map<String, dynamic> schedule) {
+  return int.tryParse(schedule['capacity'].toString()) ?? 0;
 }
